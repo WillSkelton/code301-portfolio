a8d0906988bd5f220cea3d15b3964cb8a09e0d4e
Sugarblade <William.f.skelton@outlook.com>
2016-09-26 21:06:51 -0700

    Social Bar now slides down.

---
+++ nav-script.js
@@ -0,0 +1,41 @@
+var isDisplayed = true;
+console.log(typeof(Colors.darkPurple));
+
+$('#socialButtons').css('display', 'flex');
+$('#socialButtons').css('flex-direction', 'column');
+$('#socialButtons').css('align-items', 'center');
+$('#socialButtons').css('justify-content', 'space-around');
+$('#socialButtons').css('box-sizing', 'border-box');
+$('#socialButtons').css('padding', '10px');
+$('#socialButtons').css('border', '3px solid ' + Colors.darkPurple);
+$('#socialButtons').css('width', '60px');
+$('#socialButtons').hide();
+
+
+function expandSocialIcons(){
+
+	var logoArray = $('.socialLink');
+	if (isDisplayed) {
+		isDisplayed = false;
+		$('#socialButtons').slideDown(400);
+
+	}
+	else {
+		isDisplayed = true;
+		// $('#socialButtons').hide();
+		$('#socialButtons').slideUp(400);
+
+	}
+}
+
+$('#nav-toggle').on('click', expandSocialIcons);
+
+
+
+
+
+
+
+
+
+//
