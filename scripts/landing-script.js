42cd3b303cf9f058f406607989e771ef7fac1564
Will Skelton <william.f.skelton@outlook.com>
2016-10-04 18:32:23 -0700

    Changed fade events to 0 ms and it looks better.

--- scripts/landing-script.js
+++ scripts/landing-script.js
@@ -6,14 +6,14 @@ $('#currentProgect').hide();
 $('#faq').hide();
 
 function enter() {
-	$('#mainMenu').fadeOut(1500);
-	$('#resume').fadeOut(1500);
-	$('#programs').fadeOut(1500);
-	$('#currentProgect').fadeOut(1500);
-	$('#faq').fadeOut(1500);
-	$('#landingPage').fadeOut(1500);
-	$('#nav-toggle').fadeIn(1500);
-	$('#mainMenu').fadeIn(1500);
+	$('#mainMenu').fadeOut(0);
+	$('#resume').fadeOut(0);
+	$('#programs').fadeOut(0);
+	$('#currentProgect').fadeOut(0);
+	$('#faq').fadeOut(0);
+	$('#landingPage').fadeOut(0);
+	$('#nav-toggle').fadeIn(0);
+	$('#mainMenu').fadeIn(0);
 }
 $('#landingPage').show();
 $('#enterIcon').on('click', enter);
