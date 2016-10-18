d67fc22ed8cb3b65bdf195d490807507af61e089
Will Skelton <william.f.skelton@outlook.com>
2016-10-18 15:33:42 -0700

    Added filter and added js to IIFE

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,6 +1,13 @@
-function loadProjects() {
-  for (var i = 0; i < projectData.length; i++) {
-    var html = template(projectData[i]);
-    $('#programs').append(html);
+(function(ctx) {
+  var ProjectView = {};
+  var source = $("#programTemplate").html();
+  var template = Handlebars.compile(source);
+  ProjectView.loadProjects = function(){
+    for (var i = 0; i < projectData.length; i++) {
+      var html = template(projectData[i]);
+      $('#programs').append(html);
+    }
   }
-};
+  console.log(ProjectView);
+  ctx.ProjectView = ProjectView;
+})(window)
--- scripts/projects.js
+++ scripts/projects.js
@@ -1,28 +1,36 @@
-function Project(opts) {
-	this.title = opts.title;
-	this.repoURL = opts.repoURL;
-	this.summary = opts.summary;
-	this.authorURLs = opts.authorURLs;
-	this.rank = opts.rank;
+(function(ctx) {
 
-}
+	function Project(opts) {
+		this.title = opts.title;
+		this.repoURL = opts.repoURL;
+		this.summary = opts.summary;
+		this.authorURLs = opts.authorURLs;
+		this.rank = opts.rank;
 
-Project.allProjects = [];
+	}
 
-Project.prototype.toHTML = function () {
+	Project.allProjects = [];
+
+	Project.prototype.toHTML = function () {
+		var source = $("#programTemplate").html();
+		var template = Handlebars.compile(source);
+	};
+
+	Project.load = function(projectData) {
+		Project.allProjects = projectData.map(function(element) {
+			return new Project(element);
+		});
+	};
+
+
+
 	var source = $("#programTemplate").html();
 	var template = Handlebars.compile(source);
-};
+	$('#programTemplate').remove();
+	console.log(window);
+	console.log(window.ProjectView);
+	ProjectView.loadProjects();
 
-Project.load = function(projectData) {
-	Project.allProjects = projectData.map(function(element) {
-		return new Project(element);
-	});
-};
+	ctx.Project = Project;
 
-
-
-var source = $("#programTemplate").html();
-var template = Handlebars.compile(source);
-$('#programTemplate').remove();
-loadProjects();
+})(window)
