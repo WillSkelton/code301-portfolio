c3798b929430a160ee6685ffa54c18d14d9ceae7
Will Skelton <william.f.skelton@outlook.com>
2016-10-30 13:59:08 -0700

    Server modifications

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var express = require('express'),
+var requestProxy = require('express-request-proxy'),
+  express = require('express'),
   port = process.env.PORT || 3000,
   app = express();
+
+app.get('/github/*', function(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: { Authorization: 'token ' + process.env.GITHUB_TOKEN }
+  }));
+});
 
 app.use(express.static('./'));
 
